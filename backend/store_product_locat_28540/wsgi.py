ce2cca1e6141782ca3b875e7b292811cfd32fe78
Crowdbotics <team@crowdbotics.com>
2021-07-07 16:24:12 +0000

    Initial commit.

---
+++ backend/store_product_locat_28540/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for store_product_locat_28540 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store_product_locat_28540.settings')
+
+application = get_wsgi_application()
